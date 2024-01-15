db65ddacbd30d9b5b716a5e6bd5d61688e713220
aezum19c <aezum19c@gmail.com>
2024-01-15 00:45:39 -0500

    cambios en calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -40,6 +40,9 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
 
+    def test_divide_method_fails_with_div_by_zer0(self):
+        self.assertRaises(TypeError, self.calc.divide(5,0))
+
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
         self.assertEqual(0, self.calc.multiply(1, 0))
@@ -61,6 +64,3 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertRaises(TypeError, self.calc.substract, "0", 0)
-        
-if __name__ == "__main__":  # pragma: no cover
-    unittest.main()
